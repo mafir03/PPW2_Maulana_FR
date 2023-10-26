572ba85ec50788d77e0487525afe2c8b54499460
mfr03 <maulgantengmaks@gmail.com>
2023-10-26 13:50:31 +0700

    breeze install complete

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\BukuController;
-use App\Http\Controllers\MainController;
-
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,14 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/buku', [BukuController::class, 'index']);
-Route::get('/buku/create', [BukuController::class, 'create'])->name('buku.create');
-Route::post('/buku/store', [BukuController::class, 'store'])->name('buku.store');
-Route::post('/buku/destroy/{id}', [BukuController::class, 'destroy'])->name('buku.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/buku/search', [BukuController::class, 'search'])->name('buku.search');
-
-Route::get('/buku/edit/{id}', [BukuController::class, 'edit'])->name('buku.edit');
-Route::post('/buku/update/{id}', [BukuController::class, 'update'])->name('buku.update');
-
+require __DIR__.'/auth.php';
